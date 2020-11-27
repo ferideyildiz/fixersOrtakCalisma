f89589eb83300d553d29b3585eb2dffa7594fea7
Feride <celikfeza2@gmail.com>
2020-11-27 20:02:30 +0300

    feride 2.commit

--- src/fixersOrtakCalisma/Feride.java
+++ src/fixersOrtakCalisma/Feride.java
@@ -3,5 +3,6 @@ package fixersOrtakCalisma;
 public class Feride {
     public static void main(String[] args) {
         System.out.println("19:35-Feride branch");
+        System.out.println("20:01-Feride branch");
     }
 }
